1c37ca7a119c6e71c4e31ce3fcd5e60f1799c70c
mdMERAJ77 <mmd200975@gmail.com>
2023-03-27 23:52:43 +0530

    save

---
+++ day4 rev.py
@@ -0,0 +1,18 @@
+"""x=int(input())
+y=x
+ans=0
+while x!=0:
+    remainder=x%10
+    x=x//10
+    ans=ans*10+remainder
+print(ans)
+if(ans==y):
+    print("is palidrome")
+else:
+    print("is not palidrome")
+    """
+    
+x=int(input())  
+for i in range(x):
+    print("*"*x)
+    
---
+++ day4armstrong.py
@@ -0,0 +1,16 @@
+x=int(input())
+y=t=x
+count=0
+while x!=0:
+    x=x//10
+    count=count+1
+print(count)
+ans=0
+while y!=0:
+    rem=y%10    
+    ans=ans+(rem**count)
+    y=y//10
+if (ans==t):
+    print("is armstrong")
+else:
+    print("is not armstrong")
